6133916d5bc2c2231c04f15abd976bcc3ddcf795
JaeHo Song <thruthesky@gmail.com>
2020-12-29 20:00:01 +0800

    first import

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
-# icon_animator
+# Icon Animator
 
-A new Flutter package project.
+Simple and highly configurable icon animation package with icon size, color, duration.
 
-## Getting Started
 
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
+## Example code
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
-# icon_animator
+```dart
+SizedBox(
+  width: 32,
+  height: 32,
+  child: IconAnimator(
+    icon: Icons.radio_button_unchecked,
+    loop: 1,
+    resizes: [
+      IconAnimate(size: 0, duration: 100),
+      IconAnimate(size: 4, color: Colors.red, duration: 100),
+      IconAnimate(size: 8, duration: 100),
+      IconAnimate(size: 12, color: Colors.red, duration: 100),
+      IconAnimate(size: 16, duration: 100),
+      IconAnimate(size: 20, color: Colors.blue, duration: 150),
+      IconAnimate(size: 24, color: Colors.blue, duration: 200),
+      IconAnimate(size: 28, color: Colors.blue, duration: 250),
+      IconAnimate(size: 32, color: Colors.blue, duration: 300),
+    ],
+  ),
+),
+```
--- lib/icon_animator.dart
+++ lib/icon_animator.dart
@@ -1,7 +1,64 @@
 library icon_animator;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class IconAnimate {
+  IconAnimate(
+      {@required this.size, this.color, @required this.duration, this.icon});
+  final double size;
+  final Color color;
+  final int duration;
+  final IconData icon;
+}
+
+class IconAnimator extends StatefulWidget {
+  IconAnimator({@required this.icon, @required this.resizes, this.loop});
+  final IconData icon;
+  final int loop;
+  final List<IconAnimate> resizes;
+  @override
+  _IconAnimatorState createState() => _IconAnimatorState();
+}
+
+class _IconAnimatorState extends State<IconAnimator> {
+  int frameCount = 0;
+  int loopCount = 0;
+  IconAnimate resize;
+  @override
+  void initState() {
+    super.initState();
+
+    resizeLoop();
+  }
+
+  resizeLoop() {
+    setState(() {
+      resize = widget.resizes[frameCount];
+    });
+    Timer(
+      Duration(milliseconds: widget.resizes[frameCount].duration),
+      () {
+        frameCount++;
+        if (frameCount >= widget.resizes.length) {
+          if (widget.loop != null) {
+            loopCount++;
+            if (loopCount >= widget.loop) return;
+          }
+          frameCount = 0;
+        }
+        resizeLoop();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      resize.icon != null ? resize.icon : widget.icon,
+      size: resize.size,
+      color: resize.color,
+    );
+  }
 }
